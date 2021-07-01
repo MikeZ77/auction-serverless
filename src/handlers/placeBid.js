b5bee3d1edb85d22ea529003e4ec7c627ccf54f7
Michael <mzaghi@ubuntu-20.4>
2021-07-01 14:26:57 -0700

    Processing auctions

--- src/handlers/placeBid.js
+++ src/handlers/placeBid.js
@@ -11,6 +11,10 @@ async function placeBid(event, context) {
 
   const auction = await getAuctionById(id);
 
+  if (auction.status !== 'OPEN') {
+    throw new createError.Forbidden(`You cannot biit on closed auctions!`);
+  }
+
   if (amount <= auction.highestBid.amount) {
     throw new createError.Forbidden(`You're bid must be higher than ${auction.highestBid.amount}!`);
   }
