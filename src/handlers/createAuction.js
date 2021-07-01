ff8e48d20df9f1f0cd797bdba2b6cdd58f78b895
Michael <mzaghi@ubuntu-20.4>
2021-06-30 20:59:53 -0700

    Adding placeBid endpoint and refactoring middleware

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,24 +1,35 @@
 import { v4 as uuid } from 'uuid';
 import AWS from 'aws-sdk';
+import commonMiddleware from '../lib/commonMiddleware';
+import createError from 'http-errors';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function createAuction(event, context) {
 
-  const { title } = JSON.parse(event.body);
+  const { title } = event.body;
   const now = new Date();
 
   const auction = {
     id: uuid(),
     title,
     status: 'OPEN',
-    createdAt: now.toISOString()
+    createdAt: now.toISOString(),
+    highestBid: {
+      amount: 0,
+    }
   };
 
-  await dynamodb.put({
-    TableName: process.env.AUCTIONS_TABLE_NAME,
-    Item: auction,
-  }).promise();
+  try {
+    await dynamodb.put({
+      TableName: process.env.AUCTIONS_TABLE_NAME,
+      Item: auction,
+    }).promise();
+  } catch(error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  };
+
 
   return {
     statusCode: 201,
@@ -26,6 +37,5 @@ async function createAuction(event, context) {
   };
 }
 
-export const handler = createAuction;
-
+export const handler = commonMiddleware(createAuction);
 
