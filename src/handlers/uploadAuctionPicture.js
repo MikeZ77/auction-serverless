39dea29f9afda5cb9f95c962b308571e071413a4
Michael <mzaghi@ubuntu-20.4>
2021-07-10 17:49:18 -0700

    Adding S3 image upload

---
+++ src/handlers/uploadAuctionPicture.js
@@ -0,0 +1,31 @@
+import middy from '@middy/core';
+import httpErrorHandler from '@middy/http-error-handler';
+import createError from 'http-errors';
+import { getAuctionById } from './getAuction';
+import { exportPictureToS3 } from '../lib/uploadPicture';
+import { setAuctionPictureUrl } from '../lib/setAuctionPictureUrl';
+
+export async function uploadAuctionPicture(event, context) {
+
+  const { id } = event.pathParameters;
+  const auction = await getAuctionById(id);
+  const base64 = event.body.replace(/^data:image\/\w+;base64,/, '');
+  const buffer = Buffer.from(base64, 'base64');
+
+  let updatedAuction;
+
+  try {
+    const pictureUrl = await uploadPictureToS3(auction.id + '.jpg', buffer);
+    updatedAuction = await setAuctionPictureUrl(auction.id, pictureUrl);
+  } catch (error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify(updatedAuction),
+  };
+}
+
+export const handler = middy(uploadAuctionPicture)
+  .use(httpErrorHandler());
